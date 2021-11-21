a93bed27333c7b81e89812c36b6eb9212fad9cf3
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-21 12:01:59 +0800

    Done on my intro

---
+++ program1Number Sorter.py
@@ -0,0 +1,11 @@
+# Program 1: Number Sorter
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement.
+
+print("Hello, kindly put your 4 numbers below for me to arrange it from highest to lowest")
+
+no1= float(input("Enter your first number: "))
+no2= float(input("Enter your second number: "))
+no3= float(input("Enter your third number: "))
+no4= float(input("Enter your fourth number: "))
+

175949a0a50351560ed9da5f1fb39d61cbdf8bb0
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-21 19:39:45 +0800

    So ive been trying to run this program but it keeps on getting an error so idecided to make a new approach on it

--- program1Number Sorter.py
+++ program1Number Sorter.py
@@ -2,10 +2,23 @@
 # Create a program that ask 4 numbers. 
 # Print the 4 numbers from highest to lowest using only if-else statement.
 
-print("Hello, kindly put your 4 numbers below for me to arrange it from highest to lowest")
+print("Hello, kindly put your 4 numbers below for me to arrange it from highest to lowest.")
 
 no1= float(input("Enter your first number: "))
 no2= float(input("Enter your second number: "))
 no3= float(input("Enter your third number: "))
 no4= float(input("Enter your fourth number: "))
 
+def high_to_low(num1, num2, num3, num4):
+    if high == num1:
+        if num2 > num3 and num3 > num4:
+            print(f"Thank you! Your result is as follows: {num1}, {num2}, {num3}, and {num4}.")
+        elif num2 < num3 and num3 > num4:
+            print(f"Thank you! Your result is as follows: {num1}, {num3}, {num2}, and {num4}.")
+        elif num2 < num3 and num3 < num4:
+            print(f"Thank you! Your result is as follows: {num1}, {num3}, {num4}, and {num2}.")
+        else:
+            print(f"Thank you! Your result is as follows: {num1}, {num4}, {num2}, and {num3}.")
+            return high
+
+high = high_to_low(num1 = no1, num2 = no2, num3 = no3, num4 = no4)

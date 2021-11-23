5051592c4f988a66072ea24066ef588be9cd23af
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-23 19:52:46 +0800

    So I made a sample file and try and fix there my program

--- quizzzy_addition.py
+++ quizzzy_addition.py
@@ -12,7 +12,7 @@ pg_name = "☆☆ Krizzy's Addition Quiz ☆☆"
 print(" ╔══════════ஜ۩۞۩ஜ══════════╗")
 print(pg_name)
 print(" ╚══════════ஜ۩۞۩ஜ══════════╝")
-print("\nGood day, welcome to my addition quiz. Try now to test your math skills!")
+print("\nGood day, welcome to my addition quiz. Try now, to test your math skills!")
 
 # 2. Ask the name
 player = input("\nKindly put your name here: ")
@@ -20,12 +20,12 @@ player = input("\nKindly put your name here: ")
 print(f"Hello {player}, hope you're ready for this exciting quiz!")
 print("Now, let's start the quiz!")
 
-bestScore = 10
+bestScore = 0
 pScore = 0
 
-while bestScore <= 10:
+
+while bestScore <= 9:
     bestScore += 1
-    ops = ["+"]
     rand1 = randrange(0, 99)
     rand2 = randrange(0, 99)
     sum = rand1 + rand2
@@ -34,15 +34,15 @@ while bestScore <= 10:
     ans = int(input("Your answer: "))
     if int(ans) == sum:
         pScore += 1
-        print("Excellent! Your answer is correct!")
+        print("Excellent! Your answer is correct!（*^_^*)")
     else:
-        print(f"Your answer is incorrect! The answer is {sum}")
-    bestScore = bestScore + 1
+        print(f"Your answer is incorrect! The answer is {sum}(×_×)")
+        bestScore += 1
 print(f"You got {pScore} out of {bestScore} questions correct")
 if pScore == 10:
-    print(f"Congratulations {player}! You are already expert in adding numbers!")
+    print(f"Congratulations {player}! You are already expert in adding numbers! ┍ლ(╹◡╹ლ)")
 elif pScore >= 6 and pScore <=9:
-    print(f"You've done a great job {player}! Practice more and come back to perfect this quiz!")
+    print(f"You've done a great job {player}! Practice more and come back to perfect this quiz! ㋛")
 else:
     if pScore <= 5:
         print(f"Sadly you failed the quiz {player} へ（>_<へ), I think you need to focus on adding numbers!")

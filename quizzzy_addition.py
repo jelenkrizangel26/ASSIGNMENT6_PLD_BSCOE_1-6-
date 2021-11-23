1a442ba37f25e8223d63cfc7f4a6699a31e88893
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-23 15:22:34 +0800

    I always like to put intro in my program so here it is

---
+++ quizzzy_addition.py
@@ -0,0 +1,15 @@
+# Program 2: Addition Quiz
+# Create a program that automatically generate two random numbers to add (0 to 99)
+# Let the user answer and evaluate if the user has the correct answer
+# The program will ask the user 10 addition operation
+# Display the result summary of the 10 operations (ex 9/10)
+
+# Steps
+# 1. Introduce your program
+
+from random import randrange
+pg_name = "☆☆ Krizzy's Addition Quiz ☆☆"
+print(" ╔══════════ஜ۩۞۩ஜ══════════╗")
+print(pg_name)
+print(" ╚══════════ஜ۩۞۩ஜ══════════╝")
+print("\nHello! Good day, welcome to addition quiz. Try now to test your math skills!")

93adf9da365ca117c47829f08852bbdc466cbd21
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2021-11-23 16:57:51 +0800

    I dont know but before its already okay but now its not running :>

--- quizzzy_addition.py
+++ quizzzy_addition.py
@@ -23,13 +23,29 @@ print("Now, let's start the quiz!")
 bestScore = 10
 pScore = 0
 
-quantity = 1
+
 while quantity <= 10:
+    quantity += 1
+    ops = ["+"]
     rand1 = randrange(0, 99)
     rand2 = randrange(0, 99)
     sum = rand1 + rand2
-    print(f"\n ☆☆ Question No. {quantity}☆☆ ")
+    print(f"\n☆☆ Question No. {quantity}☆☆ ")
     print(f"{str(rand1)} + {str(rand2)}")
     ans = int(input("Your answer: "))
-
+    if int(ans) == sum:
+        pScore += 1
+        print("Excellent! Your answer is correct!")
+    else:
+        print(f"Your answer is incorrect! The answer is {sum}")
+    quantity = quantity + 1
+print(f"You got {pScore} out of {bestScore} questions correct")
+if pScore == 10:
+    print(f"Congratulations {player}! You are already expert in adding numbers!")
+elif pScore >= 6 and pScore <=9:
+    print(f"You've done a great job {player}! Practice more and come back to perfect this quiz!")
+else:
+    if pScore <= 5:
+        print(f"Sadly you failed the quiz {player} へ（>_<へ), I think you need to focus on this lesson first!")
+        print("Better luck next time （*^_^*)")
 
